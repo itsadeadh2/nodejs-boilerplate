52bfe74799d82340c0acf2c00a568e3f14517b7c
Paulo Barros <paulo.alfredo.barros@gmail.com>
2019-07-01 12:03:08 -0300

    fix(async): Change Healthcheck.status to be async

--- controllers/healthcheck-controller.js
+++ controllers/healthcheck-controller.js
@@ -3,7 +3,7 @@ const HealthCheckService = require('../services/healthcheck-service')
 const HealthCheckController = {
   async status(req, res) {
     const status = await HealthCheckService.status()
-    await res.status(200).json(status)
+    res.status(200).json(status)
   }
 }
 
